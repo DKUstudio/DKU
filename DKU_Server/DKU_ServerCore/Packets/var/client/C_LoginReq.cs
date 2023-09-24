7f3f67af5dcefce0ed282d5a4794f7ac4b032cec
psg9790 <psg9790@gmail.com>
2023-09-25 00:42:38 +0900

    release test

--- DKU_Server/DKU_ServerCore/Packets/var/client/C_LoginReq.cs
+++ DKU_Server/DKU_ServerCore/Packets/var/client/C_LoginReq.cs
@@ -17,7 +17,5 @@ namespace DKU_ServerCore.Packets.var.client
         public string id;
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CommonDefine.MAX_PACKET_STRING_LENGTH)]
         public string pw;
-
-
     }
 }
