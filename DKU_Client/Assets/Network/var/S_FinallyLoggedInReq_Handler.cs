468f0697927a0beed5254372acddd5ba7507e67f
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2023-10-24 20:55:39 +0900

    new asset

--- DKU_Client/Assets/Network/var/S_FinallyLoggedInReq_Handler.cs
+++ DKU_Client/Assets/Network/var/S_FinallyLoggedInReq_Handler.cs
@@ -3,6 +3,7 @@ using DKU_ServerCore.Packets.var.server;
 using DKU_ServerCore.Packets.var.queue;
 using DKU_ServerCore.Packets;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // 완전한 로그인 성공
 public class S_FinallyLoggedInReq_Handler
@@ -14,10 +15,12 @@ public class S_FinallyLoggedInReq_Handler
         if (res.success == 0)
         {
             // TODO 여기에 인게임 씬 이동
-            UnityEngine.Debug.Log("<color=yellow>여기서 인게임 씬 이동 하세요</color>");
+            UnityEngine.Debug.Log("<color=yellow>로그인 성공, 인게임 이동!</color>");
+            SceneManager.LoadScene("MainMap");
         }
         else if (res.success == 1)
         {
+            UnityEngine.Debug.Log("<color=yellow>로그인 성공, 인게임 이동 실패!</color>");
 
         }
     }
