ab53888099760a7cb70b464690f1c33c579f0c0f
psg9790 <psg9790@gmail.com>
2023-10-25 22:44:29 +0900

    ui scale

--- DKU_Client/Assets/Scripts/Player/CamRotate.cs
+++ DKU_Client/Assets/Scripts/Player/CamRotate.cs
@@ -16,12 +16,12 @@ public class CamRotate : MonoBehaviour, IBeginDragHandler, IDragHandler
 
     Vector2 prevPoint;
     float cameraPitch = 35f;
-    
+
     float xAngle;
     float yAngle;
     float xAngleTmp;
     float yAngleTmp;
-    
+
     void Start()
     {
         xAngle = camPivot.rotation.eulerAngles.x;
@@ -39,26 +39,26 @@ public class CamRotate : MonoBehaviour, IBeginDragHandler, IDragHandler
     public void OnDrag(PointerEventData draggingPoint)
     {
         draggingPos = draggingPoint.position;
-        
+
         // this.transform.RotateAround(player.transform.position,Vector3.up, -(draggingPos.x-beginPos.x)*rotationSpeed);
         // Vector2 lookinput = (draggingPos - beginPos) * rotationSpeed * Time.deltaTime;
         // cameraPitch = Mathf.Clamp(cameraPitch - lookinput.y, 10f, 35f);
         // camPivot.localRotation = Quaternion.Euler(cameraPitch,0,0);
 
 
-        yAngle = yAngleTmp + (draggingPos.x - beginPos.x) * Screen.height/622 * rotationSpeed * Time.deltaTime;
-        xAngle = xAngleTmp - (draggingPos.y - beginPos.y) * Screen.width/1439 * rotationSpeed * Time.deltaTime;
+        yAngle = yAngleTmp + (draggingPos.x - beginPos.x) * Screen.height / 622 * rotationSpeed * Time.deltaTime;
+        xAngle = xAngleTmp - (draggingPos.y - beginPos.y) * Screen.width / 1439 * rotationSpeed * Time.deltaTime;
         if (xAngle > max_xAngle)
         {
-            Debug.Log("MAX_X");
+            //Debug.Log("MAX_X");
             xAngle = max_xAngle;
         }
         if (xAngle < min_xAngle)
-        {            
-            Debug.Log("MAX_Y");
+        {
+            //Debug.Log("MAX_Y");
             xAngle = min_xAngle;
         }
-        
-        camPivot.rotation = Quaternion.Euler(xAngle,yAngle, 0.0f);
+
+        camPivot.rotation = Quaternion.Euler(xAngle, yAngle, 0.0f);
     }
 }
