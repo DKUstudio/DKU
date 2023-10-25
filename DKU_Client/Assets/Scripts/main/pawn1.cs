0025ee9fad1e64f8e5c0e0b44c22289b0a20b840
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2023-10-26 02:01:34 +0900

    animal game

---
+++ DKU_Client/Assets/Scripts/main/pawn1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pawn1 : MonoBehaviour
+{
+    public GameObject gameButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision collisionInfo)
+    {
+        gameButton.SetActive(true);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        gameButton.SetActive(false);
+
+    }
+}
