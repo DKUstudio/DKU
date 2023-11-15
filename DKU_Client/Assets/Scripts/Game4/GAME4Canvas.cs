5fa87ef90755bd3ab0f7251be266aef2db951491
daersh <daersh@naver.com>
2023-11-15 20:29:12 +0900

    ㅇㄴㅁㅇㄴㄹㅁㅁㄹㄴ
    ㅁㅇㄴㄹㅁㄴㅇㄹㅁㄴㄹㅁㅇㄹㄴ

--- DKU_Client/Assets/Scripts/Game4/GAME4Canvas.cs
+++ DKU_Client/Assets/Scripts/Game4/GAME4Canvas.cs
@@ -8,12 +8,13 @@ public class GAME4Canvas : MonoBehaviour
     //몹 카운트, 시간
     public TMP_Text totalMob;
     public TMP_Text currentMob;
+    public GameObject playerText;
     
     void Start()
     {
         totalMob.text = gameObject.GetComponent<MobCount>().TotalMob().ToString();
         currentMob.text = gameObject.GetComponent<MobCount>().CurrentMob().ToString();
-        
+        playerText.SetActive(true);
     }
  
     public void CurrentMobMinus()
